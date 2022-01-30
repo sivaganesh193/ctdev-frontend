c531626410aef01d6adfb3df0bc438c52592a97b
jitiendran <jitiendranjiji2000@gmail.com>
2022-01-30 21:50:09 +0530

    Created : 404 error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: AppComponent},
+    { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
