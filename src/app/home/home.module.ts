c217715071db45017961832bcf4dc751612a3c85
Vinayaka Murthy Vijay <vijayvinayak23@gmail.com>
2022-02-09 16:31:36 +0530

    home page design

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider'; 
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -13,7 +17,11 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule
   ]
 })
 export class HomeModule { }
