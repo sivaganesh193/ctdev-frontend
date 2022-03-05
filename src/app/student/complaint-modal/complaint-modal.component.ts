1b7ed949e53b3c06c1653599376b86eaaa9e4066
Aanandan-T-Ma <59764564+Aanandan-T-Ma@users.noreply.github.com>
2022-03-05 11:29:37 +0530

    Complaint raising form

--- src/app/student/complaint-modal/complaint-modal.component.ts
+++ src/app/student/complaint-modal/complaint-modal.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-complaint-modal',
-  templateUrl: './complaint-modal.component.html',
-  styleUrls: ['./complaint-modal.component.scss']
+	selector: 'app-complaint-modal',
+	templateUrl: './complaint-modal.component.html',
+	styleUrls: ['./complaint-modal.component.scss']
 })
 export class ComplaintModalComponent implements OnInit {
 
-  constructor() { }
+	complaintForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor() {
+		this.complaintForm = new FormGroup({
+			complaint: new FormControl('', Validators.required)
+		});
+	}
+
+	ngOnInit(): void {
+	}
+
+	onSubmit(): void {
+		console.log(this.complaintForm.value);
+	}
+
+	getScreenWidth(): number {
+		return screen.width;
+	}
 
 }
